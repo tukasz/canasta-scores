ad808eef0c977cb7610ce40f10836c49c9d05c89
Łukasz Łukasik <tukasz@users.noreply.github.com>
2020-11-19 15:59:23 +0100

    fix(index): correct path to manifest file

---
+++ public/service-worker.js
@@ -0,0 +1,33 @@
+const cacheName = 'kanaskulator-cache-v1';
+
+const filesToCache = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(thisCacheName => {
+          if (thisCacheName !== cacheName) {
+            return caches.delete(thisCacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    (async () => {
+      return (await caches.match(e.request)) || fetch(e.request);
+    })()
+  );
+});
